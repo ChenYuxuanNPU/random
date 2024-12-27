6fddc14a8f1d7617f86b53a5e4e93b33b53eaa8e
ChenYuxuan_NPU <chenyuxuan_npu@mail.nwpu.edu.cn>
2024-12-27 10:46:02 +0800

    统计科学教师

--- 2024.12.27.py
+++ 2024.12.27.py
@@ -7,19 +7,19 @@ from func import *
 result = []
 for year in [2023, 2024]:
     result.extend(execute_sql_sentence(
-        sentence=f'select "姓名", "校名", "任教学段", "{year}" from teacher_data_0_{year} where "主教学科" = "科学"'
+        sentence=f'select "姓名", "身份证号",from teacher_data_0_{year} where "主教学科" = "科学"'
     ))
 
 print(result)
 
-# 读取 Excel 文件
-workbook = openpyxl.Workbook()
-
-# 获取活动的工作表
-ws = workbook.active
-
-for item in result:
-    ws.append(item)
-
-# 保存工作簿到文件
-workbook.save("output.xlsx")
+# # 读取 Excel 文件
+# workbook = openpyxl.Workbook()
+#
+# # 获取活动的工作表
+# ws = workbook.active
+#
+# for item in result:
+#     ws.append(item)
+#
+# # 保存工作簿到文件
+# workbook.save("output.xlsx")
